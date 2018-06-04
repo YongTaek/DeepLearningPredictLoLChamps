596fcd1fa51c44eccf3e6bd1060cbd3ea5576c8d
Jino Park <pjessesco@gmail.com>
2018-06-04 20:28:12 +0900

    Fix `tst1.py` - Add getter/setter

--- preprocess/tst1.py
+++ preprocess/tst1.py
@@ -2,8 +2,73 @@ import csv
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+_X_train = None
+_X_test = None
+_X_val = None
+_Y_train = None
+_Y_test = None
+_Y_val = None
 
-with open('participants_79.csv') as f:
+def get_X_train():
+    global _X_train
+    return _X_train
+
+def set_X_train(arr):
+    global _X_train
+    _X_train = arr
+
+def get_X_test():
+    global _X_test
+    return _X_test
+
+def set_X_test(arr):
+    global _X_test
+    _X_test = arr
+
+def get_X_val():
+    global _X_val
+    return _X_val
+
+def set_X_val(arr):
+    global _X_val
+    _X_val = arr
+
+# ---
+
+def get_Y_train():
+    global _Y_train
+    return _Y_train
+
+def set_Y_train(arr):
+    global _Y_train
+    _Y_train = arr
+
+def get_Y_test():
+    global _Y_test
+    return _Y_test
+
+def set_Y_test(arr):
+    global _Y_test
+    _Y_test = arr
+
+def get_Y_val():
+    global _Y_val
+    return _Y_val
+
+def set_Y_val(arr):
+    global _Y_val
+    _Y_val = arr
+
+def preprocess():
+
+    global _X_train
+    global _X_test
+    global _X_val
+    global _Y_train
+    global _Y_test
+    global _Y_val
+
+    f = open('participants_79.csv')
     L = [[0 for cols in range(5)] for rows in range(138)]
 
     win = []
@@ -37,11 +102,11 @@ with open('participants_79.csv') as f:
     wins = np.array(win)
     loses = np.array(lose)
 
-    X_train, X_test, Y_train, Y_test = train_test_split(wins, loses, test_size = 0.2, random_state = 1)
-    X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.2, random_state = 1)
+    _X_train, _X_test, _Y_train, _Y_test = train_test_split(wins, loses, test_size = 0.2, random_state = 1)
+    _X_train, _X_val, _Y_train, _Y_val = train_test_split(_X_train, _Y_train, test_size = 0.2, random_state = 1)
 
 
-        
+            
     
 
 
